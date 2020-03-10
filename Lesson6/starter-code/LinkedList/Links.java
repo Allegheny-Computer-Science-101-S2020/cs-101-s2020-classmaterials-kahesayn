7fd32282cce0b4946616505349c97ab0f27d9da3
Natnael G Kahesay <nati@Natnaels-MacBook-Pro.local>
2020-03-10 14:51:15 -0400

    class participation

--- Lesson6/starter-code/LinkedList/Links.java
+++ Lesson6/starter-code/LinkedList/Links.java
@@ -9,26 +9,25 @@ public class Links extends Node {
         if (index < 0)
             System.out.println("node cannot be inserted");
         else{
-            int currentIndex = 1;
-            Node current = head;
-            while(current != null && index > currentIndex){
-                current = current.getNext();
-                currentIndex++;
-            }
-            Node temp = new Node(data);
-            if (index == 0){
-                /* insert at the front.*/
-                temp.setNext(head);
-                head = temp;
-            }
-            else{
-                /* insert in the middle or at the end? */
-
-            }
-            
-            
+          int currentIndex = 1;
+          Node current = head;
+          while(current != null && index > currentIndex){
+              current = current.getNext();
+              currentIndex++;
+          }
+          Node temp = new Node(data);
+          if (index == 0){
+              /* insert at the front.*/
+              temp.setNext(head);
+              head = temp;
+          }
+          else{
+              /* insert in the middle or at the end? */
+              temp.setNext(current.getNext());
+              current.setNext(temp);
+          }
+          size++;
         }
-
     }
     public void displayNodes() {
         if(size < 1) {
@@ -40,11 +39,27 @@ public class Links extends Node {
             while(current != null){
                 System.out.print(current.getData() + "->");
                 current = current.getNext();
-            }    
+            }
             System.out.print("\n");
         }
-        
-        
-    } 
-    
+    }
+    public void removeNode(int data){
+      Node current = head;
+      if (current.getData() == data){
+        // remove head node
+        head = current.getNext();
+      } else {
+        // remove non head node
+        while(current != null && current.getNext().getData() != data){
+          current = current.getNext();
+        }
+        current.setNext(current.getNext().getNext());
+      }
+      size--;
+    }
+    public boolean findNode(int data){
+      // add logic
+      Node current = head;
+      
+    }
 }
--- Lesson6/starter-code/LinkedList/LinksStub.java
+++ Lesson6/starter-code/LinkedList/LinksStub.java
@@ -8,16 +8,23 @@ public class LinksStub {
 			int pos = 0;
 			for (int j=nodes_in_links*i; j < nodes_in_links*i + nodes_in_links; j++){
 				link.insertNode(pos, j);
+				pos++;
 			}
 			link_arr[i] = link;
 		}
 		for (int i = 0; i < link_arr.length; i++){
 			link_arr[i].displayNodes();
 		}
-		
 
-	} 
+		link_arr[0].removeNode(3);
+		link_arr[1].removeNode(5);
+		link_arr[4].removeNode(19);
 
-	
+		for (int i = 0; i < link_arr.length; i++){
+			link_arr[i].displayNodes();
+		}
+	}
+
+
 
 } //LinkedListTester
